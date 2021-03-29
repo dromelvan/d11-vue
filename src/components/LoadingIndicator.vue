1a73989bd24e4c9808a6086f987dc9d42b360b1f
dromelvan <dromelvan@aland.net>
2021-03-30 00:17:52 +0300

    Loading indicator component.

---
+++ src/components/LoadingIndicator.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-progress-circular
+    :size="size"
+    color="grey"
+    indeterminate
+  ></v-progress-circular>
+</template>
+
+<script>
+export default {
+  name: "LoadingIndicator",
+  props: {
+    size: {
+      type: Number,
+      default: 70
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-progress-circular {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 160px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100px; /* Need a specific value to work */
+}
+</style>
