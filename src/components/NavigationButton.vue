ffa32e8ef6f23bd1e5a2708fe90f770afa0d39bb
dromelvan <dromelvan@aland.net>
2021-04-03 12:29:10 +0300

    Navigation button component.

---
+++ src/components/NavigationButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-btn
+    color="white"
+    :height="height + 20"
+    :width="width + 20"
+    icon
+    dark
+    :to="to"
+    class="navigation-btn"
+  >
+    <v-img
+      v-if="direction === 'left'"
+      :height="height"
+      :width="width"
+      src="~@/assets/images/chevron-left-dark.png"
+    />
+
+    <v-img
+      v-if="direction === 'right'"
+      :height="height"
+      :width="width"
+      src="~@/assets/images/chevron-right-dark.png"
+    />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "NavigationButton",
+  props: {
+    to: Object,
+    direction: String,
+    height: {
+      type: Number,
+      default: 50
+    },
+    width: {
+      type: Number,
+      default: 50
+    }
+  }
+};
+</script>
