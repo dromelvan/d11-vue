629b94c3db65b3db6d6d4dd211d65e0458c98f6b
dromelvan <dromelvan@aland.net>
2021-04-11 00:36:37 +0300

    Dummy view for season.

---
+++ src/views/season/Season.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="season" v-if="season">
+    <p>{{ season.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Season",
+  data: () => ({
+    season: null
+  }),
+  methods: {
+    getSeason: function() {
+      new this.$d11BootApi.SeasonApi()
+        .findSeasonById(this.$route.params.id)
+        .then(result => (this.season = result));
+    }
+  },
+  mounted() {
+    this.getSeason();
+  },
+  watch: {
+    $route() {
+      this.getSeason();
+    }
+  }
+};
+</script>
