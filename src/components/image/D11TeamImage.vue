c1156049f057be61169aa023569c51e2543aae87
dromelvan <dromelvan@aland.net>
2021-04-11 00:36:14 +0300

    D11 team image component.

---
+++ src/components/image/D11TeamImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <d11-image :type="'d11-team'" :fileName="id + '.png'" :size="size" />
+</template>
+
+<script>
+export default {
+  name: "D11TeamImage",
+  components: {
+    D11Image: () => import("@/components/image/D11Image")
+  },
+  props: {
+    id: {
+      type: Number,
+      default: 1
+    },
+    size: {
+      type: String,
+      default: "default"
+    }
+  }
+};
+</script>
