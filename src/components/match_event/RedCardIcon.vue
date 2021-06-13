8f78b586f1cd35965557c97261b4737531d48868
dromelvan <dromelvan@aland.net>
2021-06-13 09:58:35 +0300

    Card icon layout fix.

--- src/components/match_event/RedCardIcon.vue
+++ src/components/match_event/RedCardIcon.vue
@@ -9,4 +9,10 @@
   background: url("~@/assets/images/match-event-sprites.png")
     ($d11-match-event-icon-height * 2) * -1 $d11-match-event-icon-height * -1;
 }
+
+.v-list-item {
+  .card-icon.red-card-icon {
+    margin-bottom: 2px;
+  }
+}
 </style>
--- src/components/match_event/YellowCardIcon.vue
+++ src/components/match_event/YellowCardIcon.vue
@@ -9,4 +9,10 @@
   background: url("~@/assets/images/match-event-sprites.png")
     ($d11-match-event-icon-height * 2) * -1 0;
 }
+
+.v-list-item {
+  .card-icon.yellow-card-icon {
+    margin-bottom: 2px;
+  }
+}
 </style>
