63265fe767a85d0e61474980519b1f06cd250818
dromelvan <dromelvan@aland.net>
2021-07-06 01:03:07 +0300

    Match week view uses new header component.

--- src/views/match_week/MatchWeek.vue
+++ src/views/match_week/MatchWeek.vue
@@ -1,6 +1,81 @@
 <template>
   <div class="match-day" v-if="matchWeek">
-    <match-week-overview-sm-and-up v-if="smAndUp" :matchWeek="matchWeek" />
+    <d11-header
+      :backgroundPictureType="'stadium'"
+      :backgroundPictureId="19"
+      :backgroundPictureAlt="'TODO'"
+      :parentLink="{
+        text: 'Season ' + matchWeek.season.name,
+        name: 'season',
+        params: { id: matchWeek.season.id }
+      }"
+      :previousLink="{
+        name: 'matchWeek',
+        params: { id: matchWeek.id - 1, tab: tab }
+      }"
+      :nextLink="{
+        name: 'matchWeek',
+        params: { id: matchWeek.id + 1, tab: tab }
+      }"
+    >
+      <div class="header-title">
+        <h1>Match Week {{ matchWeek.matchWeekNumber }}</h1>
+      </div>
+      <div class="header-subtitle">
+        <h4>Season {{ matchWeek.season.name }}</h4>
+      </div>
+
+      <div class="active-message" v-if="this.active(matchWeek.status)">
+        * This match week is active and results are not yet final.
+      </div>
+
+      <template v-if="!this.pending(matchWeek.status)">
+        <div class="horizontal">
+          <div class="emphasised">
+            Most valuable player:
+          </div>
+          <div>
+            <team-image
+              :size="'icon'"
+              :id="matchWeek.mostValuablePlayer.team.id"
+            />
+            {{ matchWeek.mostValuablePlayer.player.name }}
+            <template v-if="matchWeek.mostValuablePlayer.d11Team.id > 1">
+              ({{ matchWeek.mostValuablePlayer.d11Team.name }})
+            </template>
+            {{ matchWeek.mostValuablePlayer.points }} pts
+          </div>
+        </div>
+
+        <div class="horizontal">
+          <div class="emphasised">
+            Premier League leader:
+          </div>
+          <div>
+            <team-image
+              :size="'icon'"
+              :id="matchWeek.premierLeagueLeader.team.id"
+            />
+            {{ matchWeek.premierLeagueLeader.team.name }}
+            {{ matchWeek.premierLeagueLeader.points }} pts
+          </div>
+        </div>
+
+        <div class="horizontal">
+          <div class="emphasised">
+            D11 League leader:
+          </div>
+          <div>
+            <d11-team-image
+              :size="'icon'"
+              :id="matchWeek.d11LeagueLeader.d11Team.id"
+            />
+            {{ matchWeek.d11LeagueLeader.d11Team.name }}
+            {{ matchWeek.d11LeagueLeader.points }} pts
+          </div>
+        </div>
+      </template>
+    </d11-header>
 
     <content-section>
       <v-container class="tabs-container">
@@ -54,8 +129,9 @@ export default {
     matchWeek: null
   }),
   components: {
-    MatchWeekOverviewSmAndUp: () =>
-      import("@/views/match_week/MatchWeekOverviewSmAndUp"),
+    D11Header: () => import("@/components/header/D11Header"),
+    TeamImage: () => import("@/components/image/TeamImage"),
+    D11TeamImage: () => import("@/components/image/D11TeamImage"),
     ContentSection: () => import("@/components/ContentSection"),
     LazyMatchList: () => import("@/views/match/LazyMatchList"),
     LazyD11MatchList: () => import("@/views/d11_match/LazyD11MatchList"),
@@ -88,3 +164,9 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.emphasised {
+  min-width: 10em;
+}
+</style>
