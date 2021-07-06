e5311c9c1f59ff5c74e05f8cbb291627752b0fe2
dromelvan <dromelvan@aland.net>
2021-07-06 18:35:10 +0300

    Match week service.

---
+++ src/services/matchWeek.service.js
@@ -0,0 +1,16 @@
+import D11BootApi from "./d11BootApi";
+
+const MatchWeekService = {
+  async findMatchWeekById(matchWeekId) {
+    try {
+      const response = new D11BootApi.MatchWeekApi().findMatchWeekById(
+        matchWeekId
+      );
+      return Promise.resolve(response);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+};
+
+export default MatchWeekService;
