9ba4a4f70a76dd6e0ec756ff244de54e1d1b3256
dromelvan <dromelvan@aland.net>
2021-07-08 00:57:15 +0300

    Country image component.

---
+++ src/components/image/CountryImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <d11-image :type="'country'" :fileName="iso + '.png'" :size="size" />
+</template>
+
+<script>
+export default {
+  name: "CountryImage",
+  components: {
+    D11Image: () => import("@/components/image/D11Image")
+  },
+  props: {
+    iso: {
+      type: String,
+      default: "_unknown"
+    },
+    size: {
+      type: String,
+      default: "default"
+    }
+  }
+};
+</script>
