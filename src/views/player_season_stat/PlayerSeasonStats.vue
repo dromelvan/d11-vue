4f0eb6ff5375c7699a1ca98c1c2219f81e520ab6
dromelvan <dromelvan@aland.net>
2021-07-10 17:21:00 +0300

    Updated player season stats view header.

--- src/views/player_season_stat/PlayerSeasonStats.vue
+++ src/views/player_season_stat/PlayerSeasonStats.vue
@@ -1,6 +1,160 @@
 <template>
   <div class="player-season-stats" v-if="season">
-    <player-season-stats-overview-sm-and-up v-if="smAndUp" :season="season" />
+    <!-- Header -------------------------------->
+    <d11-header
+      :backgroundPictureType="'stadium'"
+      :backgroundPictureId="19"
+      :backgroundPictureAlt="'TODO'"
+      :parentLink="{
+        text: 'Season History',
+        name: 'seasons'
+      }"
+      :previousLink="{
+        name: 'playerSeasonStats',
+        params: {
+          seasonId: season ? season.id - 1 : 0,
+          tab: tab
+        },
+        show: season && season.id > minSeasonId()
+      }"
+      :nextLink="{
+        name: 'playerSeasonStats',
+        params: {
+          seasonId: season ? season.id + 1 : 0,
+          tab: tab
+        },
+        show: season && season.id < maxSeasonId()
+      }"
+    >
+      <template v-if="season">
+        <div class="header-title">
+          <h1>Player Stats</h1>
+        </div>
+        <div class="header-subtitle">
+          <h4>Season {{ season.name }}</h4>
+        </div>
+
+        <div class="horizontal most-valuable-player">
+          <div class="player-image">
+            <player-image
+              :size="'large'"
+              :fileName="
+                seasonSummary.top3PlayerSeasonStats[0].player.photoFileName
+              "
+            />
+          </div>
+          <div class="player-season-stats">
+            <div class="player-name">
+              <h3>{{ seasonSummary.top3PlayerSeasonStats[0].player.name }}</h3>
+            </div>
+            <div class="points-ranking">
+              <span class="points">{{
+                seasonSummary.top3PlayerSeasonStats[0].points
+              }}</span>
+              pts
+              <span class="ranking"
+                >#{{ seasonSummary.top3PlayerSeasonStats[0].ranking }}</span
+              >
+              ranking
+            </div>
+            <div v-if="finished(season.status)">
+              {{
+                seasonSummary.top3PlayerSeasonStats[0].winCount
+                  | ordinal({ includeNumber: true })
+              }}
+              MvP award
+            </div>
+            <div
+              class="team"
+              v-if="!seasonSummary.top3PlayerSeasonStats[0].team.dummy"
+            >
+              <team-image
+                :size="'tiny'"
+                :id="seasonSummary.top3PlayerSeasonStats[0].team.id"
+              />
+              {{ teamName(seasonSummary.top3PlayerSeasonStats[0].team, 20) }}
+            </div>
+            <div
+              class="d11-team"
+              v-if="!seasonSummary.top3PlayerSeasonStats[0].d11Team.dummy"
+            >
+              <d11-team-image
+                :size="'tiny'"
+                :id="seasonSummary.top3PlayerSeasonStats[0].d11Team.id"
+              />
+              {{ seasonSummary.top3PlayerSeasonStats[0].d11Team.name }}
+            </div>
+          </div>
+
+          <div class="runners-up">
+            <div class="runner-up">
+              <div class="horizontal">
+                <team-image
+                  :size="'tiny'"
+                  :id="seasonSummary.top3PlayerSeasonStats[1].team.id"
+                />
+                <h3>
+                  {{ seasonSummary.top3PlayerSeasonStats[1].player.name }}
+                </h3>
+              </div>
+              <div class="points-ranking">
+                <span class="points">{{
+                  seasonSummary.top3PlayerSeasonStats[1].points
+                }}</span>
+                pts
+                <span class="ranking"
+                  >#{{ seasonSummary.top3PlayerSeasonStats[1].ranking }}</span
+                >
+                ranking
+              </div>
+              <div
+                class="d11-team"
+                v-if="!seasonSummary.top3PlayerSeasonStats[1].d11Team.dummy"
+              >
+                <d11-team-image
+                  :size="'tiny'"
+                  :id="seasonSummary.top3PlayerSeasonStats[1].d11Team.id"
+                />
+                {{ seasonSummary.top3PlayerSeasonStats[1].d11Team.name }}
+              </div>
+            </div>
+            <div class="runner-up">
+              <div class="horizontal">
+                <team-image
+                  :size="'tiny'"
+                  :id="seasonSummary.top3PlayerSeasonStats[2].team.id"
+                />
+                <h3>
+                  {{ seasonSummary.top3PlayerSeasonStats[2].player.name }}
+                </h3>
+              </div>
+              <div class="points-ranking">
+                <span class="points">{{
+                  seasonSummary.top3PlayerSeasonStats[2].points
+                }}</span>
+                pts
+                <span class="ranking"
+                  >#{{ seasonSummary.top3PlayerSeasonStats[2].ranking }}</span
+                >
+                ranking
+              </div>
+              <div
+                class="d11-team"
+                v-if="!seasonSummary.top3PlayerSeasonStats[2].d11Team.dummy"
+              >
+                <d11-team-image
+                  :size="'tiny'"
+                  :id="seasonSummary.top3PlayerSeasonStats[2].d11Team.id"
+                />
+                {{ seasonSummary.top3PlayerSeasonStats[2].d11Team.name }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </d11-header>
+
+    <!-- <player-season-stats-overview-sm-and-up v-if="smAndUp" :season="season" /> -->
 
     <content-section>
       <v-container class="tabs-container">
@@ -44,20 +198,26 @@ export default {
   name: "PlayerSeasonStats",
   data: () => ({
     season: null,
+    seasonSummary: null,
     page: 1,
     playerSeasonStats: null
   }),
   components: {
-    PlayerSeasonStatsOverviewSmAndUp: () =>
-      import("@/views/player_season_stat/PlayerSeasonStatsOverviewSmAndUp"),
+    //    PlayerSeasonStatsOverviewSmAndUp: () =>
+    //      import("@/views/player_season_stat/PlayerSeasonStatsOverviewSmAndUp"),
+    D11Header: () => import("@/components/header/D11Header"),
+    PlayerImage: () => import("@/components/image/PlayerImage"),
+    TeamImage: () => import("@/components/image/TeamImage"),
+    D11TeamImage: () => import("@/components/image/D11TeamImage"),
     ContentSection: () => import("@/components/ContentSection"),
     LazyPlayerSeasonStatList: () =>
       import("@/views/player_season_stat/LazyPlayerSeasonStatList")
   },
   methods: {
-    getSeason: function() {
-      SeasonService.getSeason(this.$route.params.seasonId).then(result => {
-        this.season = result;
+    loadData: function() {
+      SeasonService.getSeasonData(this.$route.params.seasonId).then(result => {
+        (this.season = result.season),
+          (this.seasonSummary = result.seasonSummary);
       });
     },
     getPlayerSeasonStats: function() {
@@ -67,24 +227,14 @@ export default {
       ).then(result => (this.playerSeasonStats = result));
     }
   },
-  computed: {
-    tab: {
-      set(tab) {
-        this.$router.replace({ params: { ...this.$route.params.tab, tab } });
-      },
-      get() {
-        return this.$route.params.tab;
-      }
-    }
-  },
   mounted() {
-    this.getSeason();
+    this.loadData();
   },
   watch: {
     $route() {
       this.playerSeasonStats = null;
       this.page = 1;
-      this.getSeason();
+      this.loadData();
       this.getPlayerSeasonStats();
     },
     page() {
@@ -94,3 +244,45 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.player-image.large {
+  min-width: 200px;
+}
+
+.player-image.large {
+  padding-right: $d11-spacer;
+}
+
+.most-valuable-player {
+  .player-season-stats {
+    padding-right: 24px;
+
+    span.points {
+      font-size: 2em;
+    }
+
+    span.ranking {
+      font-size: 1.8em;
+    }
+  }
+}
+
+.runners-up {
+  .player-image {
+    margin-top: -0.4em;
+  }
+
+  .points {
+    font-size: 1.5em;
+  }
+
+  .ranking {
+    font-size: 1.4em;
+  }
+
+  .runner-up + .runner-up {
+    margin-top: $d11-large-spacer;
+  }
+}
+</style>
